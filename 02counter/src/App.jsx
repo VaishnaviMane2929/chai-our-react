673fa3603060c38db256521a93663dab1df322e1
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-09-03 23:05:24 +0530

    hooks project commit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,15 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- let Counter = 10
+  let [Counter, setCounter] = useState[10]
  
+ const addValue = () => {
+  console.log("clicked", Counter);
+  
+   Counter = Counter + 1
+  setCounter(Counter ) 
 
+  
+ }
+
+ const removeValue = () => {
+  if(Counter > 0)
+  setCounter(Counter - 1)
+ }
   return (
     <>
      <h1>chai our react</h1>
      <h2>Counter value: {Counter}</h2>
-     <button>Add value</button> <br />
-     <button>Remove value</button>
+
+     <button onClick={addValue}>
+      Add value</button> <br />
+     <button onClick={removeValue}>
+      Remove value {Counter}</button>
+     <p>footer:{Counter}</p>
       
     </>
   )
