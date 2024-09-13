30f015fd0eb3a34a1c5c8278eee15e47714e2a8e
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-09-13 22:52:09 +0530

    initial

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+
+function App() {
+  const[color, setColor] = useState("olive")
+
+  return (
+    
+     <div className="w-full h-screen duration-200" style={{backgroundColor: Color}}>
+     </div>
+    
+  )
+}
+
+export default App
