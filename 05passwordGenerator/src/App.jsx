9656269ff124f80f43e537be1a4bd2fca00292f4
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-09-16 15:56:49 +0530

    initial commit

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -4,6 +4,18 @@ import './App.css'
 function App() {
   const [length, setLength] = useState(8)
   const[numberAllowed, setNumberAllowed] = useState(false); 
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [Password, setPassword] = useState("")
+
+  const PasswordGenerator = () => {() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    
+    if(numberAllowed) str += "0123456789"
+     if(charAllowed) str += "!@#$%^&*()_+=~"
+
+
+  }, [length,numberAllowed, charAllowed, setPassword ] }
   
 
   return (
