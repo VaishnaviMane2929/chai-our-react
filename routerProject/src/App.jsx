a9580cc85b1c02c9d2b85658a76adfe2720e00f9
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-09-23 14:44:38 +0530

    initial router commit

--- routerProject/src/App.jsx
+++ routerProject/src/App.jsx
@@ -8,8 +8,7 @@ function App() {
 
   return (
     <>
-     <h1 className='bg-green-600 p-4'>React Router project</h1>
-      
+   
     </>
   )
 }
--- routerProject/src/main.jsx
+++ routerProject/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/'
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
