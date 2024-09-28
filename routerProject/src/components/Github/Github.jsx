8434f59949636cd2862b0f1a4bddb5508c6e574f
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-09-28 12:18:28 +0530

    initial commit

---
+++ routerProject/src/components/Github/Github.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Github() {
+  return (
+    <div className='text-center m-4 bg-green-600 text-white p-4 text-3xl'>Github followers</div>
+  )
+}
+
+export default Github
--- routerProject/src/main.jsx
+++ routerProject/src/main.jsx
@@ -1,47 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import React from 'react'
+import ReactDOM from 'react';
+import React from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import About from './components/About/About.jsx'
-import Home from './components/Home/home.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Home from './components/Home/home.jsx';
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Layout/>,
-//     children:[
-//       {
-//         path: "home",
-//         element: <Home/>
-
-//       },
-      
-//       {
-//         path: "about",
-//         element: <About/>
-
-//       }
-//     ]
-//   }
-// ])
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
+    <Route path='' element={<Home/>}/>
       <Route path='about' element={<About/>}/>
-      <Route path='Contact' element={<Contact/>}/>
-      <Route path='User/:userid' element={<User/>}/>
-
+      <Route path='contact' element={<Contact/>}/> 
+        <Route path='User/:userid' element={<User/>}/>
     </Route>
   )
-)
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-   <RouterProvider router={router}/>
-  </React.StrictMode>,
 );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
