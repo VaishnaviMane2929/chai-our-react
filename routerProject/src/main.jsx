c7238d50f95d463b0fc77e2740b9dc741510ab8b
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-09-28 22:33:34 +0530

    initial

--- routerProject/src/main.jsx
+++ routerProject/src/main.jsx
@@ -7,6 +7,7 @@ import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
 import Home from './components/Home/home.jsx';
+import Github from './components/Github/Github.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
       <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/> 
         <Route path='User/:userid' element={<User/>}/>
+        <Route path='github' element={<Github/>}/>
     </Route>
   )
 );
