868ce551b37d07a4dddcfd55d0ef275d581ee1e3
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-10-01 22:47:26 +0530

    initial commit

--- routerProject/src/components/Github/Github.jsx
+++ routerProject/src/components/Github/Github.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react'
 
 function Github() {
+  const [data, setData] = useEffect([])
   useEffect(() => {
     fetch('https://github.com/VaishnaviMane2929')
-  })
+    .then(Response => Response.json())
+    .then(data => {
+      console.log(data);
+      
+    })
+  
+  }, [])
   return (
     <div className='text-center m-4 bg-green-600 text-white p-4 text-3xl'>Github Followers:</div>
   )
