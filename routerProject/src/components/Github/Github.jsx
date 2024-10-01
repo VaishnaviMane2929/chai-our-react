c005e81b82df68618bfd3557dea38667aecf8c07
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-10-01 22:39:48 +0530

    initial

--- routerProject/src/components/Github/Github.jsx
+++ routerProject/src/components/Github/Github.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Github() {
+  useEffect(() => {
+    fetch('https://github.com/VaishnaviMane2929')
+  })
   return (
     <div className='text-center m-4 bg-green-600 text-white p-4 text-3xl'>Github Followers:</div>
   )
