a42d6edcd733ce26ac729180e6d06b08b6a14495
Vaishnavi Mane 2929 <vaishnavimane991@gmail.com>
2024-10-01 22:29:39 +0530

    initial commit

--- routerProject/src/components/Github/Github.jsx
+++ routerProject/src/components/Github/Github.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Github() {
   return (
-    <div className='text-center m-4 bg-green-600 text-white p-4 text-3xl'>Github followers</div>
+    <div className='text-center m-4 bg-green-600 text-white p-4 text-3xl'>Github Followers:</div>
   )
 }
 
